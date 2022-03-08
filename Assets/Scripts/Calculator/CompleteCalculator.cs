af37d1852e4ca4538f68be6a63eabe6935c22316
ShowTimeMe <62739429+ShowTimeMe@users.noreply.github.com>
2022-03-08 17:49:16 +0800

    update

--- Assets/Scripts/Calculator/CompleteCalculator.cs
+++ Assets/Scripts/Calculator/CompleteCalculator.cs
@@ -15,25 +15,31 @@ public class CompleteCalculator : MonoBehaviour
     private string[] operArray = {"/", "*", "-", "+"};
 
 
-    private int firstnum = 0;
-    private int lastnum = 0;
+    private int firstnum = 0;// 第一个计算的数字index
+    private int lastnum = 0; // 第二个计算的数字index
+    private int operIndex = 0; //运算符index
 
-    private List<string> data=new List<string>();
-    private List<string> oper=new List<string>();
+    private List<string> data;
+    private List<string> oper;
 
     private void Start()
     {
-        button.onClick.AddListener(Calculation);
+        button.onClick.AddListener(AddData);
     }
 
-    public void Calculation()
+    public void AddData()
     {
+        data=new List<string>();
+        oper=new List<string>();
+        
         formulaStr = inputField.text;
+        //添加数据
         foreach (Match match in Regex.Matches(formulaStr, selectNum))
         {
             data.Add(match.Value);
         }
 
+        //添加运算符
         foreach (var v in formulaStr)
         {
             foreach (var s in operArray)
@@ -45,6 +51,13 @@ public class CompleteCalculator : MonoBehaviour
             }
         }
 
+        // 计算  
+        for (int i = 0; i < 100; i++)
+        {
+            Calculation();
+        }
+        text.text = "计算结果是：" + data[0];
+
         foreach (var v in data)
         {
             Debug.Log("data:"+v);
@@ -53,5 +66,107 @@ public class CompleteCalculator : MonoBehaviour
         {
             Debug.Log("oper:"+v);
         }
+    }
+
+    public void Calculation()
+    {
+        for (int i = 0; i < oper.Count; i++)
+        {
+            if (IsOperation("/"))
+            {
+                FirstNum(operIndex);
+                LastNum(operIndex);
+                Debug.Log(data[firstnum] + "/" + data[lastnum] + "=========" + firstnum + "===========" + lastnum);
+                float f = Convert.ToSingle(data[firstnum]) / Convert.ToSingle(data[lastnum]);
+                Result(f.ToString());
+            }
+            else if(IsOperation("*"))
+            {
+                FirstNum(operIndex);
+                LastNum(operIndex);
+                Debug.Log(data[firstnum] + "*" + data[lastnum] + "=========" + firstnum + "===========" + lastnum);
+                float f = Convert.ToSingle(data[firstnum]) * Convert.ToSingle(data[lastnum]);
+                Result(f.ToString());
+            }
+            else if(IsOperation("+"))
+            {
+                FirstNum(operIndex);
+                LastNum(operIndex);
+                Debug.Log(data[firstnum] + "+" + data[lastnum] + "=========" + firstnum + "===========" + lastnum);
+                float f = Convert.ToSingle(data[firstnum]) + Convert.ToSingle(data[lastnum]);
+                Result(f.ToString());
+            }
+            else if(IsOperation("-"))
+            {
+                FirstNum(operIndex);
+                LastNum(operIndex);
+                Debug.Log(data[firstnum] + "-" + data[lastnum] + "=========" + firstnum + "===========" + lastnum);
+                float f = Convert.ToSingle(data[firstnum]) - Convert.ToSingle(data[lastnum]);
+                Result(f.ToString());
+            }
+        }
+    }
+    
+    /// <summary>
+    /// 是否有运算符
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public bool IsOperation(string s)
+    {
+        for (int i = 0; i < oper.Count; i++)
+        {
+            if (oper[i] == s)
+            {
+                operIndex = i;
+                Debug.Log("operIndex:==="+operIndex);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void FirstNum(int index)
+    {
+        if (index == 0)
+        {
+            firstnum = 0;
+            return;
+        }
+        for (int i = index; i >0 ; i--)
+        {
+            if (data[i] != "null")
+            {
+                firstnum = i;
+                return;
+            }
+        }
+    }
+
+    public void LastNum(int index)
+    {
+        if (index == data.Count)
+        {
+            lastnum = data.Count;
+            return;
+        }
+        
+        for (int i = index + 1; i < data.Count ; i++)
+        {
+            if (data[i] != "null")
+            {
+                lastnum = i;
+                Debug.Log("LastNum" + lastnum + "data:" + data[lastnum]);
+                return;
+            }
+        }
+    }
+
+
+    public void Result(string str)
+    {
+        data[firstnum] = str;
+        data[lastnum] = "null";
+        oper[operIndex] = "null";
     }
 }
